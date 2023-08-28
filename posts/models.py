af43d4d3a96bbe90d4ac658972697debe63011fa
sphinx-austin <austinjuniour4@gmail.com>
2023-08-28 20:55:33 +0300

    add db models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title + " | " + str(self. author)
