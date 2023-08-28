89c64a0a65acb04c3e4d587ec4fa49e444470ecf
sphinx-austin <austinjuniour4@gmail.com>
2023-08-28 22:04:18 +0300

    update

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
+from django.urls import reverse
 
 # Create your models here.
 
@@ -12,3 +13,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + " | " + str(self. author)
+    
+    def get_absolute_url(self):
+        # return reverse('article-detail', args=(str(self.id)))
+        return reverse('home')
