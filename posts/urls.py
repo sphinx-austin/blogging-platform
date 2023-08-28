afa1e199e0a80c7f92036be8fa926925ec61a81e
sphinx-austin <austinjuniour4@gmail.com>
2023-08-28 22:04:30 +0300

    update routes

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . views import HomeView, ArticleDetailView, AddPostView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('addpost/', AddPostView.as_view(), name='add-post')
 ]
