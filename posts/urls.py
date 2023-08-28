a5c905efebda112ab7e19a30faa8c06423950073
sphinx-austin <austinjuniour4@gmail.com>
2023-08-28 20:55:45 +0300

    new routes

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from . views import HomeView, ArticleDetailView, AddPostView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', HomeView.as_view(), name='home')
 ]
