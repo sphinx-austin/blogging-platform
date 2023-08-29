1b6d69ef81458826d2567705aee067a0137748a3
sphinx-austin <austinjuniour4@gmail.com>
2023-08-30 00:01:51 +0300

    update form config

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from . models import Post
-from django.views.generic import ListView, DetailView, CreateView, DeleteView
-from .forms import PostForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .forms import PostForm, EditForm
 
 
 # view here
@@ -22,3 +22,9 @@ class AddPostView(CreateView):
     template_name = 'posts/add_post.html'
     # fields = '__all__'
     # fields = ('title', 'body')
+
+class UpdatePostView(UpdateView):
+    model =  Post
+    form_class = EditForm
+    template_name = 'posts/update_post.html'
+    # fields = ['title', 'body']
