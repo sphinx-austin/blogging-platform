388d8805728e18fc56ab1041feaf1733da66aded
sphinx-austin <austinjuniour4@gmail.com>
2023-08-30 00:44:09 +0300

    add deletepost routes

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . views import HomeView, ArticleDetailView, AddPostView, UpdatePostView
+from . views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('addpost/', AddPostView.as_view(), name='add-post'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update-article'),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete-article'),
 ]
