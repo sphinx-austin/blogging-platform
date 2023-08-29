295d488dbfff252ae72883d1c4cba37ed7e8a76a
sphinx-austin <austinjuniour4@gmail.com>
2023-08-30 00:01:35 +0300

    add edit urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . views import HomeView, ArticleDetailView, AddPostView
+from . views import HomeView, ArticleDetailView, AddPostView, UpdatePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
-    path('addpost/', AddPostView.as_view(), name='add-post')
+    path('addpost/', AddPostView.as_view(), name='add-post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update-article'),
 ]
