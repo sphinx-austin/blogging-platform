0e2e5eff7f58316956368f7527f52ca32a691892
sphinx-austin <austinjuniour4@gmail.com>
2023-09-04 11:53:51 +0300

    add Category model

--- posts/models.py
+++ posts/models.py
@@ -4,10 +4,21 @@ from datetime import datetime
 from django.urls import reverse
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        # return reverse('article-detail', args=(str(self.id)))
+        return reverse('home')
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=128, default="Undefined")
     created_at = models.DateTimeField(default=datetime.now, blank=True)
     body = models.TextField()
 
